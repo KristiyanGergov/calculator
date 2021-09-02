27b0c85ecb9026845df20d2680def78c1be526f0
kristiyan.gergov <kristiyan.gergov98@gmail.com>
2021-09-02 14:39:47 +0300

    Extract the functionality into a function

--- parser/parser.go
+++ parser/parser.go
@@ -35,6 +35,7 @@ var isTokenAnOperatorPattern = "^[\\*\\+\\-\\/]$" // Matches only the characters
 
 func (*ExpressionParser) Expression(expression []string) ([]Token, error) {
 	var tokens []Token
+	var err error
 	operators := newStack()
 
 	for _, currentToken := range expression {
@@ -42,48 +43,61 @@ func (*ExpressionParser) Expression(expression []string) ([]Token, error) {
 		tokenIsAnOperator, _ := regexp.MatchString(isTokenAnOperatorPattern, currentToken)
 
 		if tokenIsAnOperand {
-			value, _ := strconv.Atoi(currentToken)
-
-			tokens = append(tokens, Token{
-				Type:  Operand,
-				Value: value,
-			})
+			tokens = handleOperandToken(currentToken, tokens)
 		} else if tokenIsAnOperator {
-			operator, ok := mapStringToOperatorType[currentToken]
-			if !ok {
-				return nil, fmt.Errorf("unknown operator: %s", currentToken)
+			tokens, operators, err = handleOperatorToken(currentToken, tokens, operators)
+
+			if err != nil {
+				return nil, err
 			}
-			previousOperator := operators.peek()
-
-			if operators.size() == 0 {
-				operators.push(operator)
-				continue
-			}
-
-			priority := priorities[operator]
-			previousPriority := priorities[previousOperator]
-
-			for operators.size() > 0 && priority <= previousPriority {
-				operators.pop()
-				tokens = append(tokens, Token{
-					Type:  Operator,
-					Value: previousOperator,
-				})
-			}
-
-			operators.push(operator)
 		} else {
 			return nil, fmt.Errorf("invalid input")
 		}
 	}
 
 	for operators.size() > 0 {
-		operator := operators.pop()
 		tokens = append(tokens, Token{
 			Type:  Operator,
-			Value: operator,
+			Value: operators.pop(),
 		})
 	}
 
 	return tokens, nil
+}
+
+func handleOperandToken(token string, tokens []Token) []Token {
+	value, _ := strconv.Atoi(token)
+
+	return append(tokens, Token{
+		Type:  Operand,
+		Value: value,
+	})
+}
+
+func handleOperatorToken(token string, tokens []Token, operators *stack) ([]Token, *stack, error) {
+	operator, ok := mapStringToOperatorType[token]
+	if !ok {
+		return nil, nil, fmt.Errorf("unknown operator: %s", token)
+	}
+
+	if operators.size() == 0 {
+		operators.push(operator)
+		return tokens, operators, nil
+	}
+
+	previousOperator := operators.peek()
+	priority := priorities[operator]
+	previousPriority := priorities[previousOperator]
+
+	for operators.size() > 0 && priority <= previousPriority {
+		operators.pop()
+		tokens = append(tokens, Token{
+			Type:  Operator,
+			Value: previousOperator,
+		})
+	}
+
+	operators.push(operator)
+
+	return tokens, operators, nil
 }
